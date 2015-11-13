8b0d93776d996fb7b89ade0305d2a90b069c8e30
wangrl <ruilingwang@163.com>
2015-11-13 17:57:33 +0800

    初始化

--- spring/src/main/java/org/wrl/spring/jdbc/dao/jdbc/UserJdbcDaoImpl.java
+++ spring/src/main/java/org/wrl/spring/jdbc/dao/jdbc/UserJdbcDaoImpl.java
@@ -1,12 +1,15 @@
 package org.wrl.spring.jdbc.dao.jdbc;
 
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.wrl.spring.jdbc.dao.IUserDao;
 import org.wrl.spring.jdbc.model.UserModel;
 
-
-public class UserJdbcDaoImpl extends SimpleJdbcDaoSupport implements IUserDao {
+/**
+ * 定义Dao实现，此处是使用Spring JDBC实现
+ * 放在dao.jdbc包里，如果有hibernate实现就放在dao.hibernate包里
+ */
+public class UserJdbcDaoImpl extends JdbcDaoSupport implements IUserDao {
 
     private static final String INSERT_SQL = "insert into test(name) values(:myName)";
     private static final String COUNT_ALL_SQL = "select count(*) from test";
@@ -14,7 +17,7 @@ public class UserJdbcDaoImpl extends SimpleJdbcDaoSupport implements IUserDao {
     
     @Override
     public void save(UserModel model) {
-        getSimpleJdbcTemplate().update(INSERT_SQL, new BeanPropertySqlParameterSource(model));
+        getJdbcTemplate().update(INSERT_SQL, new BeanPropertySqlParameterSource(model));
     }
 
     @Override
