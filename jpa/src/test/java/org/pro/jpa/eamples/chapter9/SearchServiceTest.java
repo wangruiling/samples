3b837e7f3655f5f6340987ce014d2ffc561468d3
wangrl <wangruiling.w@gmail.com>
2016-07-11 06:48:02 +0800

    项目初始化

--- jpa/src/test/java/org/pro/jpa/eamples/chapter9/SearchServiceTest.java
+++ jpa/src/test/java/org/pro/jpa/eamples/chapter9/SearchServiceTest.java
@@ -3,10 +3,15 @@ package org.pro.jpa.eamples.chapter9;
 import org.junit.Before;
 import org.junit.Test;
 import org.pro.jpa.eamples.BaseServiceTest;
+import org.pro.jpa.eamples.chapter8.entity.Department;
 import org.pro.jpa.eamples.chapter8.entity.Employee;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -22,8 +27,37 @@ public class SearchServiceTest extends BaseServiceTest{
     }
 
     @Test
-    public void findEmployee() throws Exception {
+    public void findEmployeeName() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<String> cq = cb.createQuery(String.class);
+        Root<Employee> emp = cq.from(Employee.class);
+        cq.select(emp.get("name")).distinct(true);
 
+        TypedQuery<String> tq = em.createQuery(cq);
+        System.out.println(tq.getResultList());
+    }
+
+    @Test
+    public void findEmployee() throws Exception {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Department> criteriaQuery = cb.createQuery(Department.class);
+        Root<Department> dept = criteriaQuery.from(Department.class);
+        Root<Employee> emp = criteriaQuery.from(Employee.class);
+
+        criteriaQuery.select(dept).distinct(true)
+                .where(cb.equal(dept, emp.get("department")));
+
+        List<Predicate> criteria = new ArrayList<>();
+        ParameterExpression<String> p = cb.parameter(String.class, "empName");
+        criteria.add(cb.equal(emp.get("name"), p));
+
+        criteriaQuery.where(criteria.get(0));
+
+        TypedQuery<Department> q = em.createQuery(criteriaQuery);
+        q.setParameter("empName", "Sue");
+
+        List<Department> result = q.getResultList();
+        System.out.println(result);
     }
 
     @Test
